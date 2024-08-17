5711479998fecdda153cda6d72d2836e43404078
Taylor Hulsmans <thulsmans.1133@gmail.com>
2024-08-17 14:28:55 +0100

    styes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,6 @@
 import Background from '@/assets/about-background.png'
 import Background2 from '@/assets/about-background-2.png'
+import Background3 from '@/assets/bee-theif.png' 
 export const  Home  = () => {
 
   return (<>
@@ -11,8 +12,8 @@ export const  Home  = () => {
         backgroundPosition: 'center',
         backgroundSize: 'cover',
       }}>
-      <h1>Coophive.network</h1>
-      <h2 style={{paddingTop: '5vh'}}>Empowering DePin with EAS Composables</h2>
+      <h1 style={{fontSize: '9.0vw'}}>Coophive.network</h1>
+      <h2 style={{fontSize: '3.4vw', textAlign: 'center'}}>Empowering DePin with EAS Composables</h2>
     </div>
     <div
       className="pane"
@@ -23,30 +24,54 @@ export const  Home  = () => {
         backgroundSize: 'cover',
       }}
     >
-      <div style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '85.4vw'
+      <h2 className="flex-row-wrap" style={{ alignItems: 'center', height: '23vw', fontSize: '5.5vw', textAlign: 'center'}}>About</h2>
+      <div
+        className="flex-row-wrap"
+      style={{
+        justifyContent: 'space-around',
+        width: '94.4vw',
+        flexShrink: 0,
         }}>
         <div className="card">
-          <h3>Break Cloud Monopolist Vendor Lock in</h3>
-          <p>Coophive tooling enables bringing your tech stack off the cloud and into the edge</p>
+          <h2 className="card-title">End Cloud Monopolist Vendor Lock in</h2>
+          <p className="card-content">Coophive tooling enables bringing your tech stack off the cloud and onto the edge</p>
         </div>
         <div className="card">
-          <h3>Focii for Compute Resources</h3>
-          <p>Storage, Bandwith and Processing marketplaces are heterogenous and fragmented</p>
-          <p>With EAS Attestation chaining, we can link them together pragmatically</p>
+          <h2 className="card-title">Focii for Compute Resources</h2>
+          <p className="card-content">Storage, Bandwith and Processing marketplaces are heterogenous and fragmented- with EAS Attestation chaining, we can link and exchange them together pragmatically</p>
         </div>
         <div className="card">
-          <h3>Enhancing Verifiability</h3>
-          <p>There is no silver bullet for computational verification</p>
-          <p>Though EAS Isomorphism, we gain access to an entire ecosystem of verification resources</p>
+          <h2 className="card-title">Enhancing Verifiability</h2>
+          <p className="card-content">There isno silver bullet for computational verification- through EAS Isomorphism, we gain access to an entire ecosystem of verification resources</p>
+        </div>
+      </div>
+    </div>
+    <div
+      className="pane"
+        style={{
+          backgroundImage: `url(${Background3})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+      }}>
+        <h2 className="flex-row-wrap" style={{ alignItems: 'center', height: '23vw', fontSize: '5.5vw', textAlign: 'center'}}>Partnerships</h2>
+        <div 
+          className="flex-row-wrap"
+          style={{
+          justifyContent: 'space-around',
+          width: '94.4vw',
+          }}>
+        <div className="card">
+          <h2 className="card-title">Protocol Labs</h2>
+          <p className="card-content">The future of computing is on the edge</p>
         </div>
         <div className="card">
-          <h3>Offchain Speed, Onchain Provenance</h3>
-          <p>Seemlessly bridge the trust gap between trusted and untrusted compute as needed with offchain message passing</p>
+          <h2 className="card-title">Berkeley Blockchain Accelerator</h2>
+          <p className="card-content">The future of computing is on the edge</p>
+        </div>
+        <div className="card">
+          <h2 className="card-title">Orange DAO</h2>
+          <p className="card-content">The future of computing is on the edge</p>
         </div>
       </div>
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,34 @@
-import Logo from '@/assets/coophive-logo.png'
+import Logo from '@/assets/coophive-logo-greyscale.png'
 import { Link } from 'react-router-dom'
 export const NavBar = () => {
 
   return  (
-    <div style={{
-      margin: '1vh 1vw',
+    <div
+      className="navbar"
+    style={{
+      zIndex: '9999',
+      padding: '1vh 1vw',
       position: 'fixed',
       width: '98%'
       }}>
       <div
+        className="flex-row-wrap"
         style={{
-          display:'flex',
-          flexDirection:'row',
           justifyContent:'space-between',
           alignItems:'center',
-          minHeight: '61.8px',
+          minHeight: '94.4px',
           height:'8vh',
         }}
       >
-        <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+        <div 
+          className="flex-row-wrap"
+          style={{alignItems:'center'}}>
           <Link to="/"><img style={{ height:'61.8px'}} src={Logo} alt="logo" /></Link>
           <h2 style={{padding: ' 0 1vw'}}>Coophive</h2>
         </div>
-        <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+        <div
+          className="flex-row-wrap"
+          style={{alignItems:'center'}}>
           <Link to="/about" className="navlink">About</Link>
           <a target="_blank" href="https://github.com/coophive" className="navlink">Github</a>
           <a target="_blank" href="https://x.com/CoopHive" className="navlink">Twitter</a>
